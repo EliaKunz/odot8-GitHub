26457c870074e828f679106ae110ce6a0b129908
Enrico Kunz <enricok20@gmail.com>
2014-06-18 06:34:54 -0400

    Extract todo list helper

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :find_todo_list
-  
+
   def index
   end
 
@@ -33,7 +33,7 @@ class TodoItemsController < ApplicationController
       render action: :edit
     end
   end
-  
+
   def destroy
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destroy
@@ -52,7 +52,7 @@ class TodoItemsController < ApplicationController
   def find_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
-  
+
   def todo_item_params
     params[:todo_item].permit(:content)
   end
